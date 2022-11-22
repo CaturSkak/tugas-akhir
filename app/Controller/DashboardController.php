5e2c3d86d6339073fc55c48e372be7e3f49b5014
anisamelani <anisa.melani.p84@gmail.com>
2022-11-22 10:39:10 +0700

    update

--- app/Controller/DashboardController.php
+++ app/Controller/DashboardController.php
@@ -5,9 +5,20 @@ namespace App\Controller;
 class DashboardController{
     //buat function index()
     public static function index($app, $req, $rsp, $args){
+        $username = $_SESSION['username'];
+
+        $id = $app->db->get('user_details','*',[
+            "username" => $username
+        ]);
+        $data = $app->db->select('user_details','*');
+        var_dump($data);
 
         
-        $app->view->render($rsp, 'index.html', $args);
+        $app->view->render($rsp, 'index.html', [
+            'username' => $_SESSION['username'],
+            'id'     => $id,
+            'data'   => $data
+        ]);
     }
    
         
